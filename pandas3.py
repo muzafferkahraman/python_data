01b0c4b114e8227452facc7d1eecb9762d4141d7
Muzaffer Kahraman <muzaffer.kahraman@outlook.com>
2022-10-20 10:06:13 +0300

    Create pandas3.py

---
+++ pandas3.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# Calling DataFrame constructor
+df = pd.DataFrame()
+print(df)
+
+# list of strings
+lst = ['Geeks', 'For', 'Geeks', 'is',
+			'portal', 'for', 'Geeks']
+
+lst2 = ['Geeks', 'For', 'Geeks', 'is',
+			'portal', 'for', 'Geeks']
+
+# Calling DataFrame constructor on list
+df = pd.DataFrame(lst,lst2)
+print(df)
